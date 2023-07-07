b70c8d0f34e1eeaa5eded106c6c80805c5d54968
David Mendoza <davidmendozalainez99@gmail.com>
2023-07-06 20:09:40 -0600

    configurando firebase y haciendo prueba

--- comilonga/src/lib/firebase.js
+++ comilonga/src/lib/firebase.js
@@ -1,13 +1,25 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import {
+	PUBLIC_API_KEY,
+	PUBLIC_AUTH_DOMAIN,
+	PUBLIC_PROJECT_ID,
+	PUBLIC_STORAGE_BUCKET,
+	PUBLIC_MESSAGING_SENDER_ID,
+	PUBLIC_APP_ID
+} from '$env/static/public';
 
-// Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-    // ...
+	apiKey: PUBLIC_API_KEY,
+	authDomain: PUBLIC_AUTH_DOMAIN,
+	projectId: PUBLIC_PROJECT_ID,
+	storageBucket: PUBLIC_STORAGE_BUCKET,
+	messagingSenderId: PUBLIC_MESSAGING_SENDER_ID,
+	appId: PUBLIC_APP_ID
 };
 
-export const app = initializeApp(firebaseConfig);
-export const firestore = getFirestore(app);
-export const auth = getAuth(app);
+let app = initializeApp(firebaseConfig, 'CLIENT');
+const auth = getAuth(app);
+let db = getFirestore(app);
+export { auth, db };
--- comilonga/src/routes/+page.svelte
+++ comilonga/src/routes/+page.svelte
@@ -1,14 +1,27 @@
 <script>
+	import { db } from '$lib/firebase';
+	import { collectionGroup, getDocs } from 'firebase/firestore';
+	import { onMount } from 'svelte';
+	let name = '';
+	let startDate = '';
+	onMount(async () => {
+		const testCollection = collectionGroup(db, 'test');
+		const docs = await getDocs(testCollection);
+		const mappedDocs = docs.docs.map((t) => t.data());
+		const first = mappedDocs[0];
+		name = `${first.firstName} ${first.lastName}`;
+		startDate = new Date(first.startDate).toISOString();
+	});
 </script>
 
 <div class="flex justify-center items-center flex-col">
 	<div class="hero min-h-screen bg-base-200">
 		<div class="hero-content text-center">
 			<div class="max-w-md">
-				<h1 class="text-5xl font-bold">Hello there</h1>
+				<h1 class="text-5xl font-bold">Hello {name}</h1>
 				<p class="py-6">
-					Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
-					exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+					Provident cupiditate voluptatem et in. Quaerat {startDate} ut assumenda excepturi exercitationem
+					quasi. In deleniti eaque aut repudiandae et a id nisi.
 				</p>
 				<button class="btn btn-primary">Get Started</button>
 			</div>
