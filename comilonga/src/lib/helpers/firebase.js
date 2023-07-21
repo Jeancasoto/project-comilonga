810cc121ddac5c48d41063bed0b2f6b649e6dc70
Jeanca Soto <jeancasoto@Jeancas-Meraxes.local>
2023-07-20 22:54:28 -0700

    minor fixes: no funciono copiar tu logica de fetch, pero se me ocurre antes de dormir que quizas podria ser por la estructura, por que tengo los helpers en un archivo llamado admin_helpers y no firebase

--- comilonga/src/lib/helpers/firebase.js
+++ comilonga/src/lib/helpers/firebase.js
@@ -1,8 +1,26 @@
-import { storage } from '$lib/firebase';
+import { db,storage } from '$lib/firebase';
 import { getDownloadURL, ref } from 'firebase/storage';
+import { getDoc, getDocs ,doc, collection, query} from 'firebase/firestore';
+
 
 export const getFileDownloadURL = async (path = '') => {
 	if (path === '') return '';
 	const url = await getDownloadURL(ref(storage, path));
 	return url;
 };
+
+export const getGenerals = async (generals_doc_id) => {
+    const docRef = doc(db, "generales", generals_doc_id);
+    const docSnap = await getDoc(docRef);
+    return docSnap.data();    
+}; 
+
+
+export const getGeneralDocId = async () => {
+    const docs = await getDocs(query(collection(db, "generales")));
+    let current_doc;
+    docs.forEach((doc) => {
+        current_doc = doc
+    })
+    return current_doc.id;
+}; 
