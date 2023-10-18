33f80368bb3e279478c2eb4315bace07b04576d4
David Mendoza <davidmendozalainez99@gmail.com>
2023-10-17 21:32:41 -0600

    lastest

--- comilonga/src/routes/admin/resumen/+page.svelte
+++ comilonga/src/routes/admin/resumen/+page.svelte
@@ -6,11 +6,15 @@
 	import toast from 'svelte-french-toast';
 	import { fetchAllProducts, fetchAllCategories, fetchAllMessages } from '$lib/helpers/firebase';
 	import { Chart, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
+	import dayjs from 'dayjs';
 	let products = [];
 	let categories = [];
 	let mensajes = [];
 	let mensajesParseados = [];
 	let itemsFlatList = [];
+	let screenSize;
+	let selectedMonth = 1;
+	let availableYears = [];
 
 	function findProduct(id) {
 		return products.find((product) => product.id === id);
@@ -18,8 +22,12 @@
 
 	function proccessMessages() {
 		mensajesParseados = mensajes.map((m) => {
+			const sentDate = dayjs(m.date);
+			const year = sentDate.year();
+			if (!availableYears.includes(year)) availableYears = [...availableYears, year];
 			return {
 				...m,
+				date: sentDate,
 				items: m.items.map((i) => findProduct(i.id))
 			};
 		});
@@ -37,7 +45,6 @@
 		categories = await fetchAllCategories();
 		proccessMessages();
 		createFlatList();
-		console.log(mensajesParseados);
 		toast.success('Cargado correctamente');
 	});
 
@@ -47,7 +54,7 @@
 		}),
 		datasets: [
 			{
-				label: '% of Votes',
+				label: 'Total de productos enviados',
 				data: products.map(() => {
 					return Math.floor(Math.random() * 100) + 1;
 				})
@@ -57,4 +64,25 @@
 	Chart.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
 </script>
 
-<Bar {data} options={{ responsive: true }} />
+<svelte:window bind:innerWidth={screenSize} />
+<label for="months">Select a Month:</label>
+<select id="months" bind:value={selectedMonth}>
+	{#each Array.from({ length: 12 }, (_, i) => i + 1) as month}
+		<option value={month}
+			>{new Date(2023, month - 1, 1).toLocaleString('default', { month: 'long' })}</option
+		>
+	{/each}
+</select>
+<Bar
+	{data}
+	options={{
+		responsive: true,
+		scales: {
+			x: {
+				ticks: {
+					display: screenSize > 800 //this will remove only the label
+				}
+			}
+		}
+	}}
+/>
